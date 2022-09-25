852aac274e501ed933737c93eb191e4442b8765f
grantkollar <89742140+grantkollar@users.noreply.github.com>
2022-09-25 14:04:55 -0400

    fetchmessages testing

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,8 +1,17 @@
+
 const nameInput = document.getElementById("my-name-input");
 const myMessage = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
 const chatBox = document.getElementById("chat");
+const testP = document.getElementById("test");
 
-function updateMessages(){
-  
+
+function fetchMessages(){
+  return fetch(`https://it3049c-chat-application.herokuapp.com/messages`)
+  .then(response => response.json());
 }
+async function updateMessages(){
+  const response = await fetchMessages();
+  console.log(response);
+}
+updateMessages();
