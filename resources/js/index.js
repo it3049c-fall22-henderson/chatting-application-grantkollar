970fbd4c5aad14421e3bb9164831af1c5005eebc
grantkollar <89742140+grantkollar@users.noreply.github.com>
2022-09-25 15:57:38 -0400

    Chatbox code

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,17 +1,66 @@
-
 const nameInput = document.getElementById("my-name-input");
 const myMessage = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
 const chatBox = document.getElementById("chat");
-const testP = document.getElementById("test");
-
+const serverURL = `https://it3049c-chat-application.herokuapp.com/messages`;
 
 function fetchMessages(){
-  return fetch(`https://it3049c-chat-application.herokuapp.com/messages`)
+  return fetch(serverURL)
   .then(response => response.json());
 }
 async function updateMessages(){
-  const response = await fetchMessages();
-  console.log(response);
+  const messages = await fetchMessages();
+  let formattedMessages = "";
+  messages.forEach(message => {
+      formattedMessages += formatMessage(message, nameInput.value);
+  });
+  chatBox.innerHTML = formattedMessages;
+  
 }
+function formatMessage(message, myNameInput) {
+  const time = new Date(message.timestamp);
+  const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
+
+  if (myNameInput === message.sender) {
+      return `
+      <div class="mine messages">
+          <div class="message">
+              ${message.text}
+          </div>
+          <div class="sender-info">
+              ${formattedTime}
+          </div>
+      </div>
+      `
+  } else {
+      return `
+          <div class="yours messages">
+              <div class="message">
+                  ${message.text}
+              </div>
+              <div class="sender-info">
+                  ${message.sender} ${formattedTime}
+              </div>
+          </div>
+      `
+  }
+}
+function sendMessages(username, text) {
+  const newMessage = {
+      sender: username,
+      text: text,
+      timestamp: new Date()
+  }
+
+  $.post(serverURL, newMessage);
+}
+sendButton.addEventListener("click", function(sendButtonClickEvent) {
+  sendButtonClickEvent.preventDefault();
+  const sender = nameInput.value;
+  const message = myMessage.value;
+
+  sendMessages(sender,message);
+  myMessage.value = "";
+});
 updateMessages();
+setInterval(updateMessages, 10000);
